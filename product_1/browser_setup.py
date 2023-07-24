a8ba78a86c068643cd8a3fa951c43cf3ecaaf904
Stella920904 <shyoon@stclab.com>
2023-07-24 10:45:44 +0900

    chrome버전 업데이트 관련 수정

--- product_1/browser_setup.py
+++ product_1/browser_setup.py
@@ -1,14 +1,23 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="module")
 def browser():
+    # options = Options()
     # options = webdriver.ChromeOptions()
     # options.add_argument("--window-size=1920,1080")
     # options.add_argument("--headless")
+    # driver = webdriver.Chrome(executable_path_='/Users/stclab/Documents/GitHub/STCLab_QA/drivers/chromedriver')
+   # driver = webdriver.Chrome(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    # driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.90").install())
     
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument("executable_path=/Users/stclab/Documents/GitHub/STCLab_QA/drivers/chromedriver")
+    driver = webdriver.Chrome(options=options)
+    
     yield driver
 
 
